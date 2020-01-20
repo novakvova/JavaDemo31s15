8d2d0f402495f5da8fcc7671b7dc69151dede5c2
novakvova <novakvova@gmail.com>
2020-01-20 17:25:18 +0200

    Add JCF

---
+++ jcf-demo/src/Car.java
@@ -0,0 +1,50 @@
+import java.util.Comparator;
+
+public class Car {
+	private String name;
+	private String color;
+	private double price;
+
+	public Car(String name, String color, double price) {
+		super();
+		this.name = name;
+		this.color = color;
+		this.price = price;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public void Show() {
+		System.out.printf("Name: %s\tColor%s\tPrice: %f\n", name, color, price);
+	}
+}
+
+class SortbyPrice implements Comparator<Car> 
+{ 
+    // Used for sorting in ascending order of 
+    // roll number 
+
+    public int compare(Car a, Car b) 
+    { 
+    	double price = a.getPrice() - b.getPrice();
+    	//String str= String.valueOf(price);
+        return (int)price;// Integer.parseInt(str); 
+    } 
+} 
